9eb57b8b616b75b4d81d5e5781eb200650eae80b
Iris Sanchez <sanchezorts.iris@gmail.com>
2025-02-15 21:39:27 +0100

    Fix bookcover preview

--- new_book/new_book.js
+++ new_book/new_book.js
@@ -70,3 +70,20 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     });
 
+
+const fileInput = document.getElementById('bookcover'); 
+const preview = document.getElementById('preview'); 
+
+fileInput.addEventListener('change', function(event) { 
+    const file = event.target.files[0]; 
+    if (file) { 
+        const reader = new FileReader(); 
+        reader.onload = function(e) { 
+            preview.src = e.target.result; 
+            preview.hidden = false; // Show the image preview 
+        } 
+        reader.readAsDataURL(file); // Convert the file to a data URL 
+    } else { 
+        preview.hidden = true; // Hide the preview if no file is selected 
+    } 
+})
